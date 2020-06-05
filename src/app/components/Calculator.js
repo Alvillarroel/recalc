d24271c8df03eaec1af460fe335ec675cbc36b5b
OSX <osx@MacBook-Pro-de-OSX-2.local>
2020-06-05 09:53:03 -0400

    remove hook useEffect in calculator.js component

--- src/app/components/Calculator.js
+++ src/app/components/Calculator.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 import CalculatorDisplay from './CalculatorDisplay';
 import ButtonPanel from './ButtonPanel';
